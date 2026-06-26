c95e6e9ac426c2d11eed6af9bec1f7ac01042dd6
Juliewh <yuliamaryashina@gmail.com>
2026-06-26 16:11:27 +0300

    Bank mock

--- ExternalMocks/Bank/FakeBankClient.cs
+++ ExternalMocks/Bank/FakeBankClient.cs
@@ -1,15 +1,33 @@
 namespace ExternalMocks.Bank;
 
-// Fake bank: lets tests drive the success / decline / timeout branches of payment.
 public class FakeBankClient : IBankClient
 {
+    private readonly BankOutcome _outcome;
+
+    public FakeBankClient()
+        : this(BankOutcome.Success)
+    {
+    }
+
+    public FakeBankClient(BankOutcome outcome)
+    {
+        if (outcome is BankOutcome.Undefined)
+            throw new ArgumentException("Bank outcome must be defined", nameof(outcome));
+
+        _outcome = outcome;
+    }
+
     public bool TryCharge(long clientId, decimal amount)
     {
         if (amount < 0)
             throw new ArgumentException("Amount must not be negative", nameof(amount));
 
-        // TODO (person 3): make the outcome configurable so corner-case tests can
-        // force decline and timeout, not only success.
-        return true;
+        return _outcome switch
+        {
+            BankOutcome.Success => true,
+            BankOutcome.Decline => false,
+            BankOutcome.Timeout => throw new TimeoutException("Bank did not respond in time"),
+            _ => throw new InvalidOperationException($"Unexpected bank outcome: {_outcome}"),
+        };
     }
 }
